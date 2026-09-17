c8a348b6639835e7e5ff173ed383ad500ed05157
DineshChinnasamy <dinesh1601829@gmail.com>
2026-09-17 21:25:11 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/FinanceWebSite17-09-2026/',
+  base: '/FinanceWebSite/',
 })
